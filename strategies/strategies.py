1aa84eed5f9bf36cefa5b1eb2b2e802a53f9d187
ZzzNTKzzZ <ntk22052006@gmail.com>
2026-07-09 23:49:47 +0700

    Update PPO model with market scenarios and strategies

---
+++ strategies/strategies.py
@@ -0,0 +1,8 @@
+
+class Bull(): 
+    def condition(ema_20, ema_50, slope_ema_20):
+        # Ema Pullback
+        if ema_20 > ema_50 and slope_ema_20 > 0:
+            
+
+    def ema_pullback(): 
